e5ae68e3b5d8f9bfab71f135b904551c911984d9
AbdullayevKamron <124658627+AbdullayevKamron@users.noreply.github.com>
2023-02-24 10:19:00 +0500

    IF_ELSE_ELIF
    IF_ELSE_ELIF

---
+++ app.py
@@ -0,0 +1,60 @@
+# Bolta_bormi = bool(input('Bolta_bormi:'))
+# Qilich_bormi = bool(input('Qilich_bormi:'))
+#
+# if Bolta_bormi and Qilich_bormi:
+#     print('Siz eng kuchlisiz')
+# elif Bolta_bormi or Qilich_bormi:
+#     print('Siz kuchlisiz')
+# if not Qilich_bormi:
+#     print('Sizga Qilich kerak')
+#
+#
+#
+#
+# Bolta_bormi = True
+# Qilich_bormi = True
+#
+# if Bolta_bormi and Qilich_bormi:
+#     print('Siz eng kuchlisiz')
+# if Bolta_bormi or Qilich_bormi:
+#     print('Siz kuchlisiz')
+# if not Qilich_bormi:
+#     print('Sizga Qilich kerak')
+
+
+
+
+
+
+
+
+# Bolta_bormi = True
+# Qilich_bormi = True
+#
+# if Bolta_bormi and Qilich_bormi:
+#     print('Siz eng kuchlisiz')
+
+
+
+# Bolta_bormi = True
+# Qilich_bormi = False
+#
+# if Bolta_bormi or Qilich_bormi:
+#     print('Siz kuchlisiz')
+
+
+
+# Bolta_bormi = False
+# Qilich_bormi = False
+#
+# if not Qilich_bormi:
+#     print('Sizga Qilich kerak')
+
+
+# Tekshir Bolta_bormi and Qilich_bormi: Natija Siz eng kuchlisiz
+#
+# Tekshir Bolta_bormi lekin Qilich_bormi yoq: Natija Siz kuchlisz
+#
+# Tekshir Qilich_bormi yoq: Natija Sizga Qilich kerak
+
+
